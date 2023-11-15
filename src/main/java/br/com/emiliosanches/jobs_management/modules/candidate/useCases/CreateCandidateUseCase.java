44f1c7e03ed83cddba6676eb0324c2e26faeb001
Emilio <emiliosneto13@gmail.com>
2023-11-15 15:53:29 -0300

    Hash candidate password before saving

--- src/main/java/br/com/emiliosanches/jobs_management/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/emiliosanches/jobs_management/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package br.com.emiliosanches.jobs_management.modules.candidate.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.emiliosanches.jobs_management.exceptions.UserAlreadyExistsException;
@@ -12,11 +13,17 @@ public class CreateCandidateUseCase {
   @Autowired
   private CandidateRepository candidateRepository;
 
+  @Autowired
+  PasswordEncoder passwordEncoder;
+
   public CandidateEntity execute(CandidateEntity candidateEntity) {
     this.candidateRepository
         .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail()).ifPresent((user) -> {
           throw new UserAlreadyExistsException("There's already an user with the provided username or e-mail");
         });
+
+    var password = this.passwordEncoder.encode(candidateEntity.getPassword());
+    candidateEntity.setPassword(password);
 
     return this.candidateRepository.save(candidateEntity);
   }
