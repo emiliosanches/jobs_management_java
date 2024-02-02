6cff62c1e106a6e989527a38f46009cb8f4e2c83
Emilio <emiliosneto13@gmail.com>
2024-02-02 11:23:36 -0300

    Implement authentication in swagger

--- src/main/java/br/com/emiliosanches/jobs_management/JobsManagementApplication.java
+++ src/main/java/br/com/emiliosanches/jobs_management/JobsManagementApplication.java
@@ -6,16 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-	info = @Info(
-		title = "Jobs Offers",
-		description = "A REST api for managing jobs offers and candidates",
-		version = "1.0.0"
-	)
-)
+@OpenAPIDefinition(info = @Info(title = "Jobs Offers", description = "A REST api for managing jobs offers and candidates", version = "1.0.0"))
+@SecurityScheme(name = "jwt_auth", scheme = "bearer", bearerFormat = "JWT", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class JobsManagementApplication {
 
 	public static void main(String[] args) {
